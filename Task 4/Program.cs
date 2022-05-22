d6e31608fea4a6d74591e8c166bdbc76d9fdbad7
vitassablino <105976325+vitassablino@users.noreply.github.com>
2022-05-22 12:21:24 +0300

    Добавьте файлы проекта.

---
+++ Task 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        string name, zodiakSign, jobPlace;
+        uint age, yearBorn;
+        const int yearNow = 2022;
+        Console.WriteLine("Как Вас зовут?"); name = Console.ReadLine();
+        Console.WriteLine("Сколько вам лет?"); age = Convert.ToUInt16(Console.ReadLine());
+        Console.WriteLine("Какой Ваш знак зодиака"); zodiakSign = Console.ReadLine();
+        Console.WriteLine("Где Вы работаете?"); jobPlace = Console.ReadLine();
+        yearBorn = yearNow - age;
+        Console.WriteLine($"\n\nВас зовут {name}.\nВам {age} лет.\nВы родились в {yearBorn} году.\nВаш знак зодиака - {zodiakSign}.\nВы работаете {jobPlace}");
+        }
+    }
